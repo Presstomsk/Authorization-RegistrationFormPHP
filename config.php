8a537e8e26cfda2e11bb1f1aaccc7d38d2a3a9a7
Roman Permiakov <info@presstomsk.ru>
2022-08-03 19:39:17 +0700

    Authorization Form

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    define('USER', 'root');
+    define('PASSWORD', 'root');
+    define('HOST', 'localhost');
+    define('DATABASE', 'mydb');
+    try {
+        $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+    } catch (PDOException $e) {
+        exit("Error: " . $e->getMessage());
+    }
+?>
